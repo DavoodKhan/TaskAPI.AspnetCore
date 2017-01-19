e6ba56bd41bf1a5c55e7bbbd69e13374be550c77
Davood Khan <davood.khan@appliedis.com>
2017-01-19 19:59:38 +0530

    Second attempt

---
+++ TaskAPI.AspNetCore.Web/src/TaskAPI.AspNetCore.Web/Models/Persistent/TaskContext.cs
@@ -0,0 +1,18 @@
+﻿//using Microsoft.Data.Entity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace TaskAPI.Models
+{
+    public class TaskContext: DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Task> Tasks { get; set; }
+        public DbSet<TaskList> TaskLists { get; set; }
+
+    }
+}
---
+++ TaskAPI.AspNetCore.Web/src/TaskAPI.AspNetCore.Web/TaskConfig.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TaskAPI.AspNetCore.Web
+{
+    public class TaskConfig
+    {
+        public string MongoConnString { get; set; }        
+    }
+}
